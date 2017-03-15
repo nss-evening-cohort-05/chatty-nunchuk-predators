7074af7be7d3670d4c5a0677509677a6bd13501c
webbdm <gwebb762@gmail.com>
2017-03-14 19:38:07 -0500

    added xhr functions

--- Chatty.js
+++ Chatty.js
@@ -1,6 +1,26 @@
 var SolarSystem = (function(){
 
-	var variableHere= [""];
+
+	function fileLoaded(){
+	var data = JSON.parse(this.responseText);
+	return data;
+	}
+
+	function fileFailed(){
+		console.log("failed");
+	}
+
+	var messageRequest = new XMLHttpRequest();
+	messageRequest.addEventListener("load", fileLoaded);
+	messageRequest.addEventListener("error", fileFailed);
+	messageRequest.open("GET","boilerplate.json");
+	messageRequest.send();
+	console.log("messageRequest", messageRequest);
+
+
+	var boilerplate = [];
+	boilerplate.push(data); //push(data).message;
+
 
 	return {
 
