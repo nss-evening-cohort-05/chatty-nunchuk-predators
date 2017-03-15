c333519b2d0d98bac975d70e7ecc6c8ab31b90b9
Mitchell Blom <mitchellblom@gmail.com>
2017-03-14 19:16:58 -0500

    added references and event listeners

---
+++ DOMhandler.js
@@ -0,0 +1,44 @@
+//////////////////////////////////MB/////////////////////////////////
+////////////////////References and Event Listeners //////////////////
+
+// Button references
+
+var postNewMessageButton = document.getElementById("");
+var clearAllMessagesButton = document.getElementById("");
+
+// Textbox references
+
+var userInputTextbox = document.getElementById("");
+
+// Checkbox references
+
+var darkThemeCheckbox = document.getElementById("");
+var largeTextCheckbox = document.getElementById("");
+
+// Button event listeners
+
+postNewMessageButton.addEventListener("click", function(){
+
+});
+
+clearAllMessagesButton.addEventListener("click", function(){
+
+});
+
+// Textbox event listeners
+
+userInputTextbox.addEventListener("", function(){
+
+});
+
+// Checkbox event listeners
+
+darkThemeCheckbox.addEventListener("change", function(){
+
+});
+
+largeTextCheckbox.addEventListener("change", function(){
+
+});
+
+/////////////////////////////// MB end ///////////////////////////////
