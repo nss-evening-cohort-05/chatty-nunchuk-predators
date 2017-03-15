a14570e104d25281864968a4b65a11590251aca1
Mitchell Blom <mitchellblom@gmail.com>
2017-03-14 20:36:24 -0500

    some enter button lines

--- DOMhandler.js
+++ DOMhandler.js
@@ -1,6 +1,8 @@
 //////////////////////////////////MB/////////////////////////////////
 ////////////////////References and Event Listeners //////////////////
 
+var messageBoard = {};
+
 // Button references
 
 var postNewMessageButton = document.getElementById("");
@@ -40,5 +42,15 @@ darkThemeCheckbox.addEventListener("change", function(){
 largeTextCheckbox.addEventListener("change", function(){
 
 });
+
+// Enter button
+
+document.onkeydown = function() {
+ if (window.event.keyCode === 13) {
+	// post user input text to dom
+	// messageBoard.innerHTML += userInputTextbox.value
+ }
+};
+
 
 /////////////////////////////// MB end ///////////////////////////////
